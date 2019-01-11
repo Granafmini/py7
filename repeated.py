c8bf424eea72425fde220c161ae1a6a3d1d8e00d
Granafmini <41004653+Granafmini@users.noreply.github.com>
2019-01-11 13:31:47 +0530

    Create repeated.py

---
+++ repeated.py
@@ -0,0 +1,16 @@
+def repeat(l):
+	for i in range(len(l)):
+		for j in range(i+1,len(l)):
+			if l[i]==l[j]:
+				return l[i]
+	return 'Unique'
+def main():
+	n=int(input())
+	l=[]
+	for i in range(n):
+		l.append(int(input()))
+	print(repeat(l));
+try:
+	main()
+except:
+	print('invalid');
